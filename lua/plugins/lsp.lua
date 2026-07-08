d1578340753acf12b99b223af8ee59dc52651784
The-Hungry-Caterpillar <ogata1991@gmail.com>
2026-07-08 15:23:03 -0400

    add mason to lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,7 +1,45 @@
 return {
-  "neovim/nvim-lspconfig",
-  config = function()
-    vim.lsp.config("r_language_server", {})
-    vim.lsp.enable("r_language_server")
-  end,
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "stylua",
+        "shfmt",
+        "shellcheck"
+      },
+    },
+  },
+
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "neovim/nvim-lspconfig",
+    },
+    opts = {
+      ensure_installed = {
+        "bashls",
+        "lua_ls",
+        "jsonls",
+        "yamlls",
+        "marksman",
+        -- "r_language_server", -- easier to install interatively in R
+      },
+
+    },
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      vim.lsp.enable({
+        "bashls",
+        "lua_ls",
+        "jsonls",
+        "yamlls",
+        "marksman",
+        "r_language_server",
+      })
+    end,
+  },
 }
